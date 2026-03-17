711a989cf1a175b09098985547125f273cc2118f
Ilya Strukov <ilya@istrukov.com>
2026-03-17 11:32:02 +0000

    Add missing files to git

---
+++ cmd/digestbot/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	"github.com/iley/digestbot/internal/config"
+)
+
+func main() {
+	cfg, err := config.Parse()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	b, err := bot.New(cfg.BotToken)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error creating bot: %v\n", err)
+		os.Exit(1)
+	}
+
+	_, err = b.SendMessage(context.Background(), &bot.SendMessageParams{
+		ChatID: cfg.ChatID,
+		Text:   "Hello from Digestbot!",
+		ParseMode: models.ParseModeHTML,
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error sending message: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Message sent successfully.")
+}
